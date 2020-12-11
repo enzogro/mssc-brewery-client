46bdec009b5de5bef9065d63fc05692926931f3d
Enzo Daniel Grosso Yazzar <enzogro45@gmail.com>
2020-12-11 17:32:00 +0100

    upgrading spring version

--- src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscbreweryclient;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,13 +1,15 @@
 package guru.springframework.msscbreweryclient.web.client;
 
-import guru.springframework.msscbreweryclient.web.model.BeerDto;
+
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.UUID;
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BreweryClientTest {
